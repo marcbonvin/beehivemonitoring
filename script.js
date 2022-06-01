f0cf57783408adeec35c15e1ebd86a5841935078
marcbonvin <marc.bonvin@unil.ch>
2022-06-01 16:23:12 +0200

    Functions
    adding checkfunc and drawLineChart + tests
    
    Co-Authored-By: manuu11 <81250806+manuu11@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declaration of variables
 var sch_hum, sch_temp, sch_weight, wur_hum, wur_temp, wur_weight
 
 min_date = new Date('2017-01-01 13:00:00')
@@ -42,30 +43,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     wur_weight = wur_weight.filter( a => {return new Date(a.timestamp) < max_date })
                     wur_temp = wur_temp.sort(function (a,b) {return d3.ascending(a.timestamp, b.timestamp); });
 
+    checkfunc()
+    drawLineChart()
 
-// declaration of variables
+    });
 
-var sch_weight, wur_weight
-
-// add data
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // load weight files 
-    Promise.all([d3.csv('Data/schwartau/weight_schwartau.csv'),
-                d3.csv('Data/wurzburg/weight_wurzburg.csv')])
-                    .then(function(values){
-
-                        sch_weight = values[0];
-                        wur_weight = values[1];
-
-                    })
-})
-
+});
 
 // check if the checkboxes are active or not
 
-function checkfunction(){
+function checkfunc(){
 
     // check for schwartau weight checkbox
     if (document.getElementById("sch_weight").checked == true){
@@ -79,56 +66,108 @@ function checkfunction(){
     }
     else{ d3.select('.wur_weight').attr('opacity', 0)}
 
+    // check for temperature check box
+    if (document.getElementById("temperature").checked == true){
+        d3.select('.sch_temp').attr('opacitiy', 1)
+        d3.select('.wur_temp').attr('opacitiy', 1)
+    }
+    else{ d3.select('.sch_hum').attr('opacity', 0)
+            d3.select('.wur_hum').attr('opacity', 0)}
+
 }
 
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-
-  // When reading the csv, I must format variables:
-function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-},
-
-
-function(data){
-    // from https://d3-graph-gallery.com/graph/line_basic.html
-    // append the svg object to the weight section
-
+function drawLineChart()
+{
     var svg = d3.select(".weight")
-        .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-
-    var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-    // add X axis
-
-    var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-            
+    .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', 
+            'translate(' + margin.left + ',' + margin.top +')');
     
-})
+    var x = d3.scaleTime()
+        .domain([min_date , max_date])
+        .range([ 0, width ]);
+    svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(x));
 
-    // set the ranges
-    var x = d3.scaleTime().range([0, width]);
-    var y0 = d3.scaleLinear().range([height, 0]);
-    var y1 = d3.scaleLinear().range([height, 0]);
+    // add Y axis
+    max_weight = Math.max(d3.max(wur_weight, function(d) { return parseFloat(d.weight); }) , d3.max(sch_weight, function(d) { return parseFloat(d.weight); }))
+    var y = d3.scaleLinear()
+    .domain([0, max_weight])
+    .range([ height, 0 ]);
+    svg.append('g')
+    .call(d3.axisLeft(y));
 
-    // define the 1st line
-    var valueline = d3.line()
-        .x(function(d) { return x(new Date(d.timestamp)); })
-        .y(function(d) { return y0(d.temperature); })
-        .curve(d3.curveMonotoneX);
+    function make_y_gridlines() {		
+        return d3.axisLeft(y)
+        .tickSize(-lineInnerWidth - 175)
+          .tickFormat("")
+    }
+    svg.append('g')			
+    .attr('class', 'grid')
+    .attr('transform','translate('+ 0 +','+ 0 +')')
+    .call(make_y_gridlines()
+    )
 
-    // define the 2nd line
-    var valueline2 = d3.line()
-    .x(function(d) { return x(new Date(d.timestamp)); })
-    .y(function(d) { return y1(d.humidity); }).curve(d3.curveMonotoneX)
+    var prev;
+    // Add the line
+    svg.append('path')
+    .datum(sch_weight)
+    .attr('class' , 'sch_weight')
+    .attr('fill', 'none')
+    .attr('stroke', 'red')
+    .attr('stroke-width', 1.5)
+    .attr('d', d3.line()
+       .x(function(d) { return x(new Date(d.timestamp)) })
+       .y(function(a) { if (a.weight == 0)
+        {
+            // console.log(a.weight)
+            return y((prev))
+        }
+        else{
+            prev = a.weight;
+            return y((a.weight)); 
+        }})
+        .curve(d3.curveMonotoneX)
+       )
+
+        prev = 0;
+    svg.append('path')
+    .datum(wur_weight)
+    .attr('class' , 'wur_weight')
+    .attr('fill', 'none')
+    .attr('stroke', 'blue')
+    .attr('stroke-width', 1.5)
+    .attr('d', d3.line()
+       .x(function(d) { return x(new Date(d.timestamp)) })
+       .y(function(a) { if (a.weight == 0)
+        {
+            // console.log(a.weight)
+            return y((prev))
+        }
+        else{
+            prev = a.weight;
+           return y((a.weight)); 
+        }})
+        .curve(d3.curveMonotoneX)
+       )
+
+    //Append y-axis label
+    svg.append('text').attr('class' , 'axisLabel')
+            .attr('text-anchor', 'middle')
+            .attr('transform-origin', 'center')       
+            .attr('transform', 'rotate(-90), translate('+ 0 +','+ 0 +')')
+            .attr('y', -350)
+            .attr('x', 650)
+            .text('Weight')
+
+    //Append legend
+    svg.append('circle').attr('cx',width/4).attr('cy',-50).attr('r', 6).style('fill', 'blue')
+    svg.append('circle').attr('cx',width - width/4).attr('cy',-50).attr('r', 6).style('fill', 'red')
+    svg.append('text').attr('x', width/4 + 20).attr('y', -50).text('Weight Wuzburg').style('font-size', '15px').attr('alignment-baseline','middle')
+    svg.append('text').attr('x', width - width/4 + 20).attr('y', -50).text('Weight Schwartau').style('font-size', '15px').attr('alignment-baseline','middle')
+
+}
