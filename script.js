4f6f178800dc8a10f528885307ec5679265d82f6
marcbonvin <marc.bonvin@unil.ch>
2022-06-03 10:06:32 +0200

    functions + html
    adding checkfunct2 and drawlinechart2
    
    Co-Authored-By: manuu11 <81250806+manuu11@users.noreply.github.com>

--- script.js
+++ script.js
@@ -76,6 +76,26 @@ function checkfunc(){
 
 }
 
+function checkfunc2(){
+
+    //check for Schwartau check box
+    if (document.getElementById('Schwartau').checked == true){
+        d3.select('.sch_hum').attr('opacity' , 1)
+        d3.select('.sch_temp').attr('opacity' , 1)
+    }
+    else{  d3.select('.sch_hum').attr('opacity' , 0)
+            d3.select('.sch_temp').attr('opacity' , 0)}
+
+    //check for Wurzburg check box
+    if (document.getElementById('Wurzburg').checked == true){
+        d3.select('.wur_hum').attr('opacity' , 1)
+        d3.select('.wur_temp').attr('opacity' , 1)
+    }
+    else{  d3.select('.wur_hum').attr('opacity' , 0)
+            d3.select('.wur_temp').attr('opacity' , 0)}
+
+}
+
 function drawLineChart()
 {
     var svg = d3.select(".weight")
@@ -169,5 +189,139 @@ function drawLineChart()
     svg.append('circle').attr('cx',width - width/4).attr('cy',-50).attr('r', 6).style('fill', 'red')
     svg.append('text').attr('x', width/4 + 20).attr('y', -50).text('Weight Wuzburg').style('font-size', '15px').attr('alignment-baseline','middle')
     svg.append('text').attr('x', width - width/4 + 20).attr('y', -50).text('Weight Schwartau').style('font-size', '15px').attr('alignment-baseline','middle')
+
+}
+
+function drawLineChart2()
+{
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y0 = d3.scaleLinear().range([height, 0]);
+    var y1 = d3.scaleLinear().range([height, 0]);
+
+// define the 1st line
+    var valueline = d3.line()
+        .x(function(d) { return x(new Date(d.timestamp)); })
+        .y(function(d) { return y0(d.temperature); })
+        .curve(d3.curveMonotoneX);
+
+    // define the 2nd line
+    var valueline2 = d3.line()
+    .x(function(d) { return x(new Date(d.timestamp)); })
+    .y(function(d) { return y1(d.humidity); }).curve(d3.curveMonotoneX);
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select(".temp_hum").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+          
+          
+    // Scale the range of the data
+    x.domain([min_date , max_date]);
+    max_hum =  Math.max(d3.max(wur_hum, function(d) { return parseFloat(d.humidity); }) , d3.max(sch_hum, function(d) { return parseFloat(d.humidity); }))
+    max_temp =  Math.max(d3.max(wur_temp, function(d) { return parseFloat(d.temperature); }) , d3.max(sch_temp, function(d) { return parseFloat(d.temperature); }))
+    y0.domain([0, max_temp]);
+    y1.domain([0, max_hum]);
+
+    // Add the X Axis
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    // Add the Y0 Axis
+    svg.append("g")
+    .attr("class", "axisSteelBlue")
+    .call(d3.axisLeft(y0));
+
+    // Add the Y1 Axis
+    svg.append("g")
+    .attr("class", "axisRed")
+    .attr("transform", "translate( " + width + ", 0 )")
+    .call(d3.axisRight(y1));
+
+    //append lines for wurzburg
+    //temperature line
+    svg.append("path")
+    .datum(wur_temp)
+    .attr("class", "wur_temp")
+    .attr("fill", "none")
+    .attr("stroke", "green")
+    .attr("d", valueline);
+
+    //humidity line
+    svg.append("path")
+    .datum(wur_hum)
+    .attr("class", "wur_hum")
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("d", valueline2);
+
+
+      //append lines for schwartau
+    //temperature line
+    svg.append("path")
+    .datum(sch_temp)
+    .attr("class", "sch_temp")
+    .attr("fill", "none")
+    .attr("stroke", "blue")
+    .attr("d", valueline);
+
+    //humidity line
+    svg.append("path")
+    .datum(sch_hum)
+    .attr("class", "sch_hum")
+    .attr("fill", "none")
+    .attr("stroke", "yellow")
+    .attr("d", valueline2);
+
+ //append y0 axis label
+
+ svg.append("text").attr("class" , "axisLabel")
+   .attr("text-anchor", "middle")
+   .attr("transform-origin", "center")       
+   .attr("transform", "rotate(-90), translate("+ 0 +","+ 0 +")")
+  .attr("y", -350)
+  .attr("x", 650)
+    .text('Temperature')
+
+   //append y1 axis label
+
+ svg.append("text").attr("class" , "axisLabel")
+ .attr("text-anchor", "middle")
+ .attr("transform-origin", "center")       
+ .attr("transform", "rotate(-90), translate("+ 0 +","+ 0 +")")
+ .attr("y",750)
+ .attr("x",650)
+ .text('Humidity')
+
+  //Add gridlines
+  function make_y_gridlines() {		
+    return d3.axisLeft(y0)
+    .tickSize(-lineInnerWidth - 175)
+      .tickFormat("")
+  }
+  svg.append("g")			
+  .attr("class", "grid")
+  .attr("transform","translate("+ 0 +","+ 0 +")")
+  .call(make_y_gridlines()
+  )
+
+  //APPEND LEGEND
+  svg.append("text").attr("x", width/4 + 20).attr("y", -60).text("Temperature Wuzburg").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg.append("circle").attr("cx",width/4).attr("cy",-60).attr("r", 6).style("fill", "green")
+  
+  svg.append("circle").attr("cx",width - width/4).attr("cy",-60).attr("r", 6).style("fill", "red")
+  svg.append("text").attr("x", width - width/4 + 20).attr("y", -60).text("Humidity Wuzburg").style("font-size", "15px").attr("alignment-baseline","middle")
+  
+  svg.append("text").attr("x", width/4 + 20).attr("y", -40).text("Temperature Schwartau").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg.append("circle").attr("cx",width/4).attr("cy",-40).attr("r", 6).style("fill", "blue")
+  
+  svg.append("circle").attr("cx",width - width/4).attr("cy",-40).attr("r", 6).style("fill", "yellow")
+  svg.append("text").attr("x", width - width/4 + 20).attr("y", -40).text("Humidity Schwartau").style("font-size", "15px").attr("alignment-baseline","middle")
 
 }
